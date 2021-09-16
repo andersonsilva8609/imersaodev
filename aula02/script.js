e6e751d82fb1a0fed1368d25d396b560e8669ca4
Anderson da Silva <andersonsilva8609@gmail.com>
2021-09-16 00:51:23 -0300

    update arquivo script.js

--- aula02/script.js
+++ aula02/script.js
@@ -1,16 +1,18 @@
 function Converter() {
   var valorElemento = document.getElementById("valor");
   var valor = valorElemento.value;
+  
   var valorEmDolarNumerico = parseFloat(valor);
   var valorEmBitcoinNumerico = parseFloat(valor);
-  var valorEmReal = valorEmDolarNumerico * 5.22;
-  var valorBitcoin = valorEmBitcoinNumerico * 251.562;  
-  
+  //Multiplicado o valor unitário 
+  var valorEmReal = valorEmDolarNumerico * 5.22; //valor de 1 dólar
+  var valorBitcoin = valorEmBitcoinNumerico * 251.562; //valor de 1 bitcoin
+
   var elementoValorConvertido = document.getElementById("valorConvertido");
-  var valorConvertido = "O resultado em real é R$ " + valorEmReal;
+  var valorConvertido = "O resultado em real é R$ " + valorEmReal.toFixed(2);
   elementoValorConvertido.innerHTML = valorConvertido;
 
   var valorBitcoinConvertido = document.getElementById("bitcoinConvertido");
-  var bitcoinConvertido = "Bitcoin em reais é R$ " + valorBitcoin;
+  var bitcoinConvertido = "Bitcoin em reais é R$ " + valorBitcoin.toFixed(2);
   valorBitcoinConvertido.innerHTML = bitcoinConvertido;
 }
