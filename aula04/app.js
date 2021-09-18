2a9a1063141c444862560b26f663156a69e1228f
Anderson da Silva <andersonsilva8609@gmail.com>
2021-09-18 17:28:32 -0300

    aula04 app.js/ index.html/ style.css - Aluraflix

---
+++ aula04/app.js
@@ -0,0 +1,10 @@
+var listaFilmes = ["https://br.web.img2.acsta.net/medias/nmedia/18/86/96/34/20028591.jpg", 
+                  "https://upload.wikimedia.org/wikipedia/pt/thumb/6/61/ScaryMovie2.jpg/200px-ScaryMovie2.jpg", 
+                  "https://br.web.img3.acsta.net/pictures/21/04/14/19/06/3385237.jpg", 
+                  "https://br.web.img3.acsta.net/medias/nmedia/18/87/30/63/19874134.jpg", 
+                  "http://es.web.img3.acsta.net/pictures/20/09/18/13/18/1949807.jpg"
+                ];
+                
+for(var i = 0; i < listaFilmes.length; i++){
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
